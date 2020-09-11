dce04fb72e6812e6f55eff883d98fdc155a031a0
Suechon <ss.710@i.softbank.jp>
2020-09-12 06:43:30 +0900

    かるく編集した

---
+++ src/components/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-container>
+      <v-col>
+        <h1>
+          <v-icon large color="black">mdi-paw</v-icon>このページについて
+        </h1>
+        <p>Qiitaでかく記事のデモなどがあります</p>
+        <h1>
+          <v-icon large color="black">mdi-paw</v-icon>その他発言場所
+        </h1>
+        <ul>
+          <li>
+            <a href="https://qiita.com/Sue_chan">Qiita</a>
+          </li>
+          <li>
+            <a href="https://skane26.hatenablog.com/">メモ書きブログ</a>
+          </li>
+        </ul>
+        <h1>
+          <v-icon large color="black">mdi-paw</v-icon>デモ
+        </h1>
+        <ul>
+          <li>
+            <a href="https://suechon.github.io/vue-alert/">vue-alert</a>
+          </li>
+        </ul>
+      </v-col>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
